74d47b39a43c4600a6ea6389a0e8c938496bdea7
ari <ari@mascii.org>
2020-03-20 16:44:38 -0400

    make compile script agnostic to antlr minor version

--- antlr/scripts/compile.sh
+++ antlr/scripts/compile.sh
@@ -4,4 +4,4 @@ rm -rf generated/*
 mkdir -p generated/{source,classes}
 antlr -package com.kastkode.mascii2.antlrgenerated -o generated/source MasciiLexer.g4 MasciiParser.g4
 #antlr -package com.kastkode.mascii2.antlrgenerated -o generated/source Mascii.g4 
-javac -cp $(brew --prefix)/Cellar/antlr/4.7.2/antlr-4.7.2-complete.jar -d generated/classes generated/source/Mascii*.java
+javac -cp $(brew --prefix)/Cellar/antlr/4.*/antlr-4.*-complete.jar -d generated/classes generated/source/Mascii*.java
